2f1538244b7bba27d3ee5ef161ee335f5398f1ed
Leandro Santos <jose.martins@accountfy.com>
2023-06-25 14:37:05 -0300

    add: food controller

---
+++ src/main/java/com/cardapio/controllers/FoodController.java
@@ -0,0 +1,23 @@
+package com.cardapio.controllers;
+
+import com.cardapio.entities.FoodEntity;
+import com.cardapio.repositories.FoodRepository;
+import com.cardapio.services.food.FoodService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/foods")
+public class FoodController {
+    @Autowired
+    private FoodService foodService;
+
+    @GetMapping("/")
+    public List<FoodEntity> readAll(){
+        return foodService.readAll();
+    }
+}
