7ccf5a9fff3b43d8225f6da4df649c0b89ec7ffd
Leandro Santos <jose.martins@accountfy.com>
2023-06-25 16:50:28 -0300

    add: using Dto on food service

--- src/main/java/com/cardapio/services/food/FoodService.java
+++ src/main/java/com/cardapio/services/food/FoodService.java
@@ -1,5 +1,7 @@
 package com.cardapio.services.food;
 
+import com.cardapio.dto.food.FoodRequestDTO;
+import com.cardapio.dto.food.FoodResponseDTO;
 import com.cardapio.entities.FoodEntity;
 import com.cardapio.repositories.FoodRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,7 +14,13 @@ public class FoodService {
     @Autowired
     private FoodRepository foodRepository;
 
-    public List<FoodEntity> readAll(){
-        return foodRepository.findAll();
+    public List<FoodResponseDTO> readAll(){
+        List<FoodResponseDTO> foodDTOList =  foodRepository.findAll().stream().map(FoodResponseDTO::new).toList();
+        return foodDTOList;
+    }
+
+    public void create(FoodRequestDTO foodRequestDTO){
+        FoodEntity foodEntity = new FoodEntity(foodRequestDTO);
+        foodRepository.save(foodEntity);
     }
 }
